e032433eae8e93c68c26f9a09ff5af40deaf148e
hiroraba <hiroraba@gmail.com>
2014-01-05 20:17:54 +0900

    Fix

--- rails.sh
+++ rails.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Install Ruby
+sudo apt-get -y update
 sudo apt-get -y install git-core
 sudo apt-get -y install libopenssl-ruby
 git clone git://github.com/sstephenson/rbenv.git .rbenv
@@ -12,8 +13,8 @@ rbenv install 2.0.0-p195
 rbenv global 2.0.0-p195
 rbenv rehash
 sudo apt-get -y install rubygems
-sudo gem install rubygems-update --no-ri --no-rdoc
-~/.rbenv/versions/2.0.0-p195/bin/gem/update_rubygems
+#sudo gem install rubygems-update --no-ri --no-rdoc
+#~/.rbenv/versions/2.0.0-p195/bin/gem/update_rubygems
 
 sudo gem install bundler
 bundle install

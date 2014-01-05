e68f0227f4bdbcd3f777653c889b30613aac0e77
hiroraba <xornelius0313@gmail.com>
2014-01-05 21:24:50 +0900

    Update rails.sh

--- rails.sh
+++ rails.sh
@@ -6,8 +6,8 @@ sudo apt-get -y install git-core
 sudo apt-get -y install libopenssl-ruby
 git clone git://github.com/sstephenson/rbenv.git .rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-which rbenv || echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> .bash_profile
-which rbenv || echo 'eval "$(rbenv init -)"' >> .bash_profile
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> .bash_profile
+echo 'eval "$(rbenv init -)"' >> .bash_profile
 source .bash_profile
 rbenv install 2.0.0-p195
 rbenv global 2.0.0-p195

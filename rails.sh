1b3ab3251a94d078740960d57de019e4e919bb43
hiroraba <xornelius0313@gmail.com>
2014-01-06 23:23:06 +0900

    Update rails.sh

--- rails.sh
+++ rails.sh
@@ -8,19 +8,8 @@ sudo apt-get -y install build-essential
 sudo apt-get -y install libreadline-dev libssl-dev zlib1g-dev
 sudo apt-get -y install libssl-dev libssl1.0.0
 sudo apt-get -y install curl
+sudo apt-get -yV install  ruby1.9.1-full
 
-git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
-
-export PATH="$HOME/.rbenv/bin:$PATH"
-rbenv init -
-
-git clone git://github.com/sstephenson/ruby-build.git
-cd ruby-build
-sudo ./install.sh
-
-rbenv install 1.9.3-p125
-rbenv global 1.9.3-p125
-rbenv rehash
 wget http://rubyforge.org/frs/download.php/76729/rubygems-1.8.25.tgz
 tar xzf rubygems-1.8.25.tgz
 cd rubygems-1.8.25

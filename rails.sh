9d66f749910974b908a6a928b4513fea07ea7404
hiroraba <xornelius0313@gmail.com>
2014-01-07 01:59:15 +0900

    Update rails.sh

--- rails.sh
+++ rails.sh
@@ -26,6 +26,7 @@ wget http://rubyforge.org/frs/download.php/76729/rubygems-1.8.25.tgz
 tar xzf rubygems-1.8.25.tgz
 cd rubygems-1.8.25
 ruby setup.rb
+cd ~
 gem install bundler
 cd Her
 bundle install

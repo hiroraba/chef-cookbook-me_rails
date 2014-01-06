1db5315b2c78112adadb40e9abf4610b5d77cadf
hiroraba <xornelius0313@gmail.com>
2014-01-06 18:51:22 +0900

    Update rails.sh

--- rails.sh
+++ rails.sh
@@ -7,15 +7,13 @@ sudo apt-get -y install libopenssl-ruby
 
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-export PATH="$HOME/.rbenv/shims:$PATH"
-rbenv init -
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+echo 'export PATH="$HOME/.rbenv/shims:$PATH"' >> ~/.bash_profile
+source ~/.bash_profile
 rbenv install 1.9.3-p125
 rbenv global 1.9.3-p125
 rbenv rehash
 wget http://rubyforge.org/frs/download.php/76729/rubygems-1.8.25.tgz
 tar xzf rubygems-1.8.25.tgz
 cd rubygems-1.8.25
-~/.rbenv/versions/1.9.3-p125/bin/ruby setup.rb
-
-~/.rbenv/versions/1.9.3-p125/bin/gem install bundler
-~/.rbenv/versions/1.9.3-p125/bin/bundle install
+ruby setup.rb

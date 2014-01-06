2032ad7755b6f440502c5d29e58aee3a83dbe67f
hiroraba <xornelius0313@gmail.com>
2014-01-06 18:39:59 +0900

    Update rails.sh

--- rails.sh
+++ rails.sh
@@ -7,7 +7,7 @@ sudo apt-get -y install libopenssl-ruby
 
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-export PATH="$HOME/.rbenv/bin:$PATH"
+export PATH="$HOME/.rbenv/shims:$PATH"
 rbenv init -
 rbenv install 1.9.3-p125
 rbenv global 1.9.3-p125

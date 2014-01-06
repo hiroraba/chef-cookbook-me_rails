36edd3c653bba8359926e00ca001e5c7f0672706
hiroraba <xornelius0313@gmail.com>
2014-01-07 00:38:39 +0900

    Update rails.sh

--- rails.sh
+++ rails.sh
@@ -11,12 +11,11 @@ sudo apt-get -y install libssl-dev libssl1.0.0
 sudo apt-get -y install curl
 
 git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export RBENV_ROOT="$HOME/.rbenv"' >> ~/.bash_profile
 echo 'export PATH="$RBENV_ROOT/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 source .bash_profile
-
-git clone git@github.com:sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 rbenv install 1.9.3-p125
 rbenv global 1.9.3-p125
